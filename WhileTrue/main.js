c31985d0dc8907bbbbd4643438eadc7e063260b1
Barto2228 <89597497+Barto2228@users.noreply.github.com>
2022-07-05 10:23:08 +0200

    Add files via upload

---
+++ WhileTrue/main.js
@@ -0,0 +1,10 @@
+// zdefiniowanie funkcji
+function  buy(){
+    // utworzenie zmiennych email i  log i z htmlela otrzymujemy wartość
+    let buy = document.getElementById("buy").value;
+    // potwierdzeie zakupu
+    alert("Witaj" + " " + "gra zakupjona" + " " + buy )
+}
+
+// wywołanie funkcji
+buy()
